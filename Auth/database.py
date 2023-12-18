bf320bcd2f8cd28923fc384cf81cdaed41cf4726
JuliosJunk <wendissweihrs@gmail.com>
2023-12-19 00:28:02 +0500

    fix/Finished_db_correction

--- Auth/database.py
+++ Auth/database.py
@@ -29,13 +29,9 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
+
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
