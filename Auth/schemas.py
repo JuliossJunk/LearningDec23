d6bd451f2391060b658509c14ea1bd5203fb8a83
JuliosJunk <wendissweihrs@gmail.com>
2023-12-19 19:34:46 +0500

    fix/Updated_schemas_for_actual

--- Auth/schemas.py
+++ Auth/schemas.py
@@ -5,7 +5,7 @@ from fastapi_users import schemas
 from fastapi_users.schemas import PYDANTIC_V2
 
 
-class UserRead(schemas.BaseUser[uuid.UUID]):
+class UserRead(schemas.BaseUser[int]):
     id: int
     email: str
     username: str
@@ -14,29 +14,25 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
     is_superuser: bool = False
     is_verified: bool = False
 
-    if PYDANTIC_V2:  # pragma: no cover
-        model_config = ConfigDict(from_attributes=True)  # type: ignore
-    else:  # pragma: no cover
-
-        class Config:
-            orm_mode = True
+    class Config:
+        from_attributes = True
 
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
     email: str
     password: str
-    role_id: str
+    role_id: int
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
 
 # for updating user information
-# class UserUpdate(schemas.BaseUserUpdate):
-#     username: Optional[str] = None
-#     password: Optional[str] = None
-#     email: Optional[str] = None
-#     role_id: Optional[str] = None
-#     is_active: Optional[bool] = None
-#     is_superuser: Optional[bool] = None
-#     is_verified: Optional[bool] = None
+class UserUpdate(schemas.BaseUserUpdate):
+    username: Optional[str] = None
+    password: Optional[str] = None
+    email: Optional[str] = None
+    role_id: Optional[str] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_verified: Optional[bool] = None
